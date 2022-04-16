934e4479a415d9e75e7bae64f83093b0e8652939
dhaydt <1992dayat@gmail.com>
2022-04-16 23:34:12 +0700

    fix footer

--- resources/views/layouts/front-end/partials/_footer.blade.php
+++ resources/views/layouts/front-end/partials/_footer.blade.php
@@ -87,26 +87,26 @@
                         <li class="widget-list-item">
                             <a class="widget-list-link"
                                href="{{route('flash-deals',[$flash_deals['id']])}}">
-                                {{\App\CPU\translate('flash_deal')}}
+                                {{\App\CPU\translate('flash_deal_room')}}
                             </a>
                         </li>
                     @endif
-                    <li class="widget-list-item"><a class="widget-list-link"
+                    {{-- <li class="widget-list-item"><a class="widget-list-link"
                                                     href="{{route('products',['data_from'=>'featured','page'=>1])}}">{{\App\CPU\translate('featured_products')}}</a>
-                    </li>
+                    </li> --}}
+                    {{-- <li class="widget-list-item"><a class="widget-list-link"
+                                                    href="{{route('products',['data_from'=>'latest','page'=>1])}}">{{\App\CPU\translate('latest_rooms')}}</a>
+                    </li> --}}
+                    {{-- <li class="widget-list-item"><a class="widget-list-link"
+                                                    href="{{route('products',['data_from'=>'best-selling','page'=>1])}}">{{\App\CPU\translate('best_selling_room')}}</a>
+                    </li> --}}
                     <li class="widget-list-item"><a class="widget-list-link"
-                                                    href="{{route('products',['data_from'=>'latest','page'=>1])}}">{{\App\CPU\translate('latest_products')}}</a>
-                    </li>
-                    <li class="widget-list-item"><a class="widget-list-link"
-                                                    href="{{route('products',['data_from'=>'best-selling','page'=>1])}}">{{\App\CPU\translate('best_selling_product')}}</a>
-                    </li>
-                    <li class="widget-list-item"><a class="widget-list-link"
-                                                    href="{{route('products',['data_from'=>'top-rated','page'=>1])}}">{{\App\CPU\translate('top_rated_product')}}</a>
+                                                    href="{{route('products',['data_from'=>'top-rated','page'=>1])}}">{{\App\CPU\translate('top_rated_room')}}</a>
                     </li>
 
-                    <li class="widget-list-item"><a class="widget-list-link"
+                    {{-- <li class="widget-list-item"><a class="widget-list-link"
                                                     href="{{route('brands')}}">{{\App\CPU\translate('all_brand')}}</a>
-                    </li>
+                    </li> --}}
                     <li class="widget-list-item"><a class="widget-list-link"
                                                     href="{{route('categories')}}">{{\App\CPU\translate('all_category')}}</a>
                     </li>
@@ -119,14 +119,14 @@
 
             <!-- Grid column -->
             <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h6 class="text-uppercase mb-4 font-weight-bold footer-heder">{{\App\CPU\translate('account&shipping_info')}}</h6>
+                <h6 class="text-uppercase mb-4 font-weight-bold footer-heder">{{\App\CPU\translate('account_&_booking_info')}}</h6>
                 @if(auth('customer')->check())
                     <ul class="widget-list" style="padding-bottom: 10px">
                         <li class="widget-list-item"><a class="widget-list-link"
                                                         href="{{route('user-account')}}">{{\App\CPU\translate('profile_info')}}</a>
                         </li>
                         <li class="widget-list-item"><a class="widget-list-link"
-                                                        href="{{route('wishlists')}}">{{\App\CPU\translate('wish_list')}}</a>
+                                                        href="{{route('wishlists')}}">{{\App\CPU\translate('favorite')}}</a>
                         </li>
                         {{--<li class="widget-list-item">
                             <a class="widget-list-link"
@@ -134,11 +134,11 @@
                             </a>
                         </li>--}}
                         <li class="widget-list-item"><a class="widget-list-link"
-                                                        href="{{route('track-order.index')}}">{{\App\CPU\translate('track_order')}}</a>
+                                                        href="{{route('track-order.index')}}">{{\App\CPU\translate('my_kost')}}</a>
                         </li>
-                        <li class="widget-list-item"><a class="widget-list-link"
+                        {{-- <li class="widget-list-item"><a class="widget-list-link"
                                                         href="{{ route('account-address') }}">{{\App\CPU\translate('address')}}</a>
-                        </li>
+                        </li> --}}
                         <li class="widget-list-item"><a class="widget-list-link"
                                                         href="{{ route('account-tickets') }}">{{\App\CPU\translate('support_ticket')}}</a>
                         </li>
@@ -154,7 +154,7 @@
                                                         href="{{route('customer.auth.login')}}">{{\App\CPU\translate('profile_info')}}</a>
                         </li>
                         <li class="widget-list-item"><a class="widget-list-link"
-                                                        href="{{route('customer.auth.login')}}">{{\App\CPU\translate('wish_list')}}</a>
+                                                        href="{{url('wishlists')}}">{{\App\CPU\translate('favorite')}}</a>
                         </li>
                         {{--<li class="widget-list-item">
                             <a class="widget-list-link"
@@ -162,11 +162,11 @@
                             </a>
                         </li>--}}
                         <li class="widget-list-item"><a class="widget-list-link"
-                                                        href="{{route('track-order.index')}}">{{\App\CPU\translate('track_order')}}</a>
+                                                        href="{{url('account-kost')}}">{{\App\CPU\translate('my_kost')}}</a>
                         </li>
-                        <li class="widget-list-item"><a class="widget-list-link"
+                        {{-- <li class="widget-list-item"><a class="widget-list-link"
                                                         href="{{route('customer.auth.login')}}">{{\App\CPU\translate('address')}}</a>
-                        </li>
+                        </li> --}}
                         <li class="widget-list-item"><a class="widget-list-link"
                                                         href="{{route('customer.auth.login')}}">{{\App\CPU\translate('support_ticket')}}</a>
                         </li>
