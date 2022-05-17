340abfb2f9c9d795b572114d8712af8aeb26710d
dhaydt <1992dayat@gmail.com>
2022-05-17 08:28:25 +0700

    fix deposit

--- resources/views/admin-views/order/order-details.blade.php
+++ resources/views/admin-views/order/order-details.blade.php
@@ -341,6 +341,9 @@
                                             <div class="col-12 col-md-8 pl-4 d-flex justify-content-between">
                                                 <span>Poin : </span><span class="text-success"> - {{ \App\CPU\Helpers::currency_converter($order->details[0]->poin) }}</span>
                                             </div>
+                                            <div class="col-12 col-md-8 pl-4 d-flex justify-content-between">
+                                                <span>Deposit : </span><span class="text-danger"> + {{ \App\CPU\Helpers::currency_converter(json_decode($order->details[0]->product_details)->deposit) }}</span>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-md-4">
